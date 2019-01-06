6f4665d67c3d5d19c1e5cbff3b0e823d21652c27
lmenna <louismenna@yahoo.com>
2019-01-05 21:01:12 -0500

    Added more markets in the Poloniex arbitrage comparison

--- arbmon/src/app.js
+++ arbmon/src/app.js
@@ -1,13 +1,15 @@
 require("@babel/polyfill");
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var threshold = 1.05;
+var threshold = 1.01;
 var numberOfChecks = 0;
 var intervalHandel = -1;
 var maxBuyArb = 0;
 var maxSellArb = 0;
+var maxSellArbETH = 0;
+var maxSellArbXMR = 0;
 
-const timeInSecondsBetweenPriceChecks = 30;
+const timeInSecondsBetweenPriceChecks = 15;
 
 function getPricingData() {
 
@@ -25,9 +27,17 @@ function getPricingData() {
     if (this.readyState===4 && this.status===200) {
       let exchangeData = xmlhttp.responseText;
       numberOfChecks++;
+      let timeStamp = new Date();
       let exchangeObject = JSON.parse(exchangeData);
-      analyzePrices(exchangeObject, "USDC");
-      analyzePrices(exchangeObject, "USDT");
+      let coins = ["FOAM", "ZEC", "LTC", "ETH", "XRP", "STR", "XMR", "DOGE", "BCHABC", "BCHSV"];
+      let baseStableCoin = "USDC";
+      analyzePrices(exchangeObject, baseStableCoin, coins, timeStamp);
+      coins = ["BAT", "BNT", "DASH", "DOGE", "EOS", "ETC", "ETH", "GNT", "KNC", "LOOM", "LSK",
+        "LTC", "MANA", "NXT", "QTUM", "REP", "SC", "SNT", "STR", "XMR", "XRP", "ZEC", "ZRX"];
+      baseStableCoin = "USDT"; 
+      analyzePrices(exchangeObject, baseStableCoin, coins, timeStamp);
+      analyzeETHPrices(exchangeObject, timeStamp);
+      analyzeXMRPrices(exchangeObject, timeStamp);
       if (numberOfChecks===1 || numberOfChecks%5===0) {
         if (intervalHandel!=-1)
           clearInterval(intervalHandel);
@@ -41,19 +51,10 @@ function getPricingData() {
   console.log("END: getPricingData");
 }
 
-function analyzePrices(exchangePrices, baseStableCoin) {
+function analyzePrices(exchangePrices, baseStableCoin, coins, timeStamp) {
 
-  var coins;
-  if (baseStableCoin==="USDC") 
-    coins = ["FOAM", "ZEC", "LTC", "ETH", "XRP", "STR", "XMR", "DOGE", "BCHABC", "BCHSV"];
-  else
-    coins = ["BAT", "BNT", "DASH", "DOGE", "EOS", "ETC", "ETH", "GNT", "KNC", "LOOM", "LSK",
-      "LTC", "MANA", "NXT", "QTUM", "REP", "SC", "SNT", "STR", "XMR", "XRP", "ZEC", "ZRX"];
-
-  let timeStamp = new Date();
   let timeStampStr = timeStamp.getTime();
-  
-  console.log(`priceCheckCount:${numberOfChecks}|maxBuyArb:${maxBuyArb}|maxSellArb:${maxSellArb}`);
+  console.log(`priceCheckCount:${numberOfChecks}|${baseStableCoin}|maxBuyArb:${maxBuyArb}|maxSellArb:${maxSellArb}`);
   // Check if buying the coin will be profitable.
   coins.forEach(curCoin => {
     let lowestAskBTC = exchangePrices["BTC_" + curCoin].lowestAsk;
@@ -65,7 +66,8 @@ function analyzePrices(exchangePrices, baseStableCoin) {
       maxBuyArb = ArbRatio;
       showMax = "NewMax";
     }
-    console.log(`REC|${timeStamp}|${timeStampStr}|Buy|${baseStableCoin}|${curCoin}|ArbRatio:${ArbRatio}|${showMax}`);
+    if (ArbRatio>1.0)
+      console.log(`REC|${timeStamp}|${timeStampStr}|Buy|${baseStableCoin}|${curCoin}|ArbRatio:${ArbRatio}|${showMax}`);
     if (ArbRatio > threshold) {
       console.log("Something dramatic needs to happen!");
     }
@@ -83,13 +85,76 @@ function analyzePrices(exchangePrices, baseStableCoin) {
       maxSellArb = ArbRatio;
       showMax = "NewMax";
     }
-    console.log(`REC|${timeStamp}|${timeStampStr}|Sell|${baseStableCoin}|${curCoin}|ArbRatio:${ArbRatio}|${showMax}`);
+    if (ArbRatio>1.0)
+      console.log(`REC|${timeStamp}|${timeStampStr}|Sell|${baseStableCoin}|${curCoin}|ArbRatio:${ArbRatio}|${showMax}`);
     if (ArbRatio > threshold) {
       console.log("Something dramatic needs to happen!");
     }
   });
 }
 
+function analyzeETHPrices(exchangePrices, timeStamp) {
+
+  let timeStampStr = timeStamp.getTime();
+  console.log(`priceCheckCount:${numberOfChecks}|ETH|maxBuyArb:N/A|maxSellArbETH:${maxSellArbETH}`);
+  let coins = ["BAT", "BNT", "CVC", "EOS", "ETC", "GAS", "GNT", "KNC", "LOOM", "LSK", 
+    "MANA", "OMG", "QTUM", "REP", "SNT", "STEEM", "ZEC", "ZRX"];
+  // Check if selling the coin will be profitable
+  coins.forEach(curCoin => {
+    let ETH_curCoinBid = exchangePrices["ETH_"+curCoin].highestBid;
+    let BTC_ETHBid = exchangePrices["BTC_ETH"].highestBid;
+    let BTC_curCoinAsk = exchangePrices["BTC_"+curCoin].lowestAsk;
+    let AmtInit = 1;
+    let AmtFinal = AmtInit*BTC_ETHBid*ETH_curCoinBid/BTC_curCoinAsk;
+    let ArbRatio = AmtFinal/AmtInit;
+    let showMax = "";
+    if (ArbRatio>maxSellArbETH) {
+      maxSellArbETH = ArbRatio;
+      showMax = "NewMax";
+    }
+    if (ArbRatio>1.0)
+      console.log(`REC|${timeStamp}|${timeStampStr}|Sell|${curCoin}|ETH|ArbRatio:${ArbRatio}|${showMax}`);
+    if (ArbRatio > threshold) {
+      let instructions = `ALERT: Sell ${AmtInit} ${curCoin} for ${AmtInit*ETH_curCoinBid} ETH, 
+        then sell those ETH for ${AmtInit*ETH_curCoinBid*BTC_ETHBid} BTC,
+        then use those BTC to buy ${AmtFinal} ${curCoin}`;
+      console.log(instructions);
+    }
+
+  });
+}
+
+
+function analyzeXMRPrices(exchangePrices, timeStamp) {
+
+  let timeStampStr = timeStamp.getTime();
+  console.log(`priceCheckCount:${numberOfChecks}|XMR|maxBuyArb:N/A|maxSellArbXMR:${maxSellArbXMR}`);
+  let coins = ["LTC", "ZEC", "NXT", "DASH", "BCN", "MAID"];
+  // Check if selling the coin will be profitable
+  coins.forEach(curCoin => {
+    let baseMarket = "XMR";
+    let baseMarket_curCoinBid = exchangePrices[baseMarket + "_" + curCoin].highestBid;
+    let BTC_baseMarketBid = exchangePrices["BTC" + "_" + baseMarket].highestBid;
+    let BTC_curCoinAsk = exchangePrices["BTC" + "_" + curCoin].lowestAsk;
+    let AmtInit = 1;
+    let AmtFinal = AmtInit*BTC_baseMarketBid*baseMarket_curCoinBid/BTC_curCoinAsk;
+    let ArbRatio = AmtFinal/AmtInit;
+    let showMax = "";
+    if (ArbRatio>maxSellArbXMR) {
+      maxSellArbXMR = ArbRatio;
+      showMax = "NewMax";
+    }
+    if (ArbRatio>1.0)
+      console.log(`REC|${timeStamp}|${timeStampStr}|Sell|${curCoin}|XMR|ArbRatio:${ArbRatio}|${showMax}`);
+    if (ArbRatio > threshold) {
+      let instructions = `ALERT: Sell ${AmtInit} ${curCoin} for ${AmtInit*baseMarket_curCoinBid} XMR, 
+        then sell those XMR for ${AmtInit*BTC_baseMarketBid*baseMarket_curCoinBid} BTC,
+        then use those BTC to buy ${AmtFinal} ${curCoin}`;
+      console.log(instructions);
+    }
+
+  });
+}
 
 /* findPricingAnomolies
  * desc: Called periodically from setInterval.  Will check through the market pricing data and notify
